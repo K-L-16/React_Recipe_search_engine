fa43cba23dbef97935e264b828f3fb2f62392d05
Kevin L <kevinliu20210000@gmail.com>
2025-11-05 09:27:31 -0600

    create nav, pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Home from './pages/home'
+import Favorites from './pages/favorites'
+import Details from './pages/details'
 
 function App() {
 
   return (
     <div>
-      
+      <div className='min-h-screen p-6 bg-white text-grey-600 text-lg'>
+        <Navbar />
+        
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/recipe-item/:id' element={<Details />} />
+        </Routes>
+      </div>
+
     </div>
   )
 }
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,10 @@
+
+
+
+
+export default function Navbar() {
+    return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap:0">
+        <h2 className="text-2 font-semibold">FoodRecipe</h2>
+        
+    </nav>
+}
